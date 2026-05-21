f64792569194951cca6a5704774a3c2239bfe44d
Anurag Sen <118921364+Rosenmunda@users.noreply.github.com>
2026-05-21 17:04:21 +0530

    Update footer link to LinkedIn

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
 					Built by{" "}
 					<a
-						href='https://github.com/Rosenmunda'
+						href='https://www.linkedin.com/in/anurag-sen19/'
 						target='_blank'
 						className='font-medium underline underline-offset-4'
 					>
